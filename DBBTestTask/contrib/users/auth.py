21f8c5c9f01c8907d0bebdf99991e507b5f05555
oleksii.kozlov <oleksii.kozlov@anvileight.com>
2024-10-18 19:40:41 +0300

    Add user model and authentication

---
+++ DBBTestTask/contrib/users/auth.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+
+import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from sqlmodel import Session
+
+from db import get_session
+from DBBTestTask import settings
+from DBBTestTask.contrib.users.models import User
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/users/login")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+
+# Create JWT access token
+def create_access_token(data: dict, expires_delta: timedelta| None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)):
+    credentials_exception = HTTPException(
+        status_code=401, detail="Could not validate credentials"
+    )
+
+    # Decode the token and get the username (sub)
+    payload = decode_token(token)
+    username: str = payload.get("sub")
+    if username is None:
+        raise credentials_exception
+
+    # Query the user from the database using the session
+    user = session.query(User).filter(User.username == username).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
+
---
+++ DBBTestTask/contrib/users/models.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True, min_length=3, max_length=50)
+    # TODO: enforce validation
+    email: EmailStr = Field(index=True, unique=True)
+    hashed_password: str = Field(min_length=60, max_length=128)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    is_active: bool = Field(default=True)
+    is_superuser: bool = Field(default=False)
+
+
+# Pydantic models
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    email: str | None
+
+class UserData(BaseModel):
+    id: int | None
+    username: str
+    email: str | None
+    is_active: bool
+
+    class Config:
+        from_attributes = True
+
+class UserInDB(UserCreate):
+    hashed_password: str
+
+class UserLoginRequest(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str
